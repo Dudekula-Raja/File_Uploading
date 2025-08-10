789eb996590c3278101c26356495f903b768dc0f
D.Rajahussain <165311647+Dudekula-Raja@users.noreply.github.com>
2025-08-10 14:32:25 +0530

    Update ImageController.java

--- src/main/java/com/fileupload/controller/ImageController.java
+++ src/main/java/com/fileupload/controller/ImageController.java
@@ -1,4 +1,4 @@
-package com.thiruacademy.controller;
+package com.fileupload.controller;
 
 import java.io.IOException;
 
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.thiruacademy.service.ImageService;
+import com.fileupload.service.ImageService;
 
 @RestController
 @RequestMapping("/imageOrFile")
